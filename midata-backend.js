7afc0336a53d7ff0a2d92ec970ce1b365d7ea495
Gabriel Hess <heg2@bfh.ch>
2020-06-30 15:49:39 +0200

    Use FHIR R4

--- midata-backend.js
+++ midata-backend.js
@@ -30,89 +30,89 @@ module.exports = {
 	token : function() {
 		return midataSettings.token;
 	},
-	
+
 	/** Get language of logged in user */
 	language : function() {
 		return midataSettings.language;
 	},
-	
+
 	/** Get URL of Midata backend */
 	server : function() {
 		return midataSettings.server;
 	},
-		 
+
 	/** Get id of logged in user */
 	userId : function() {
 		return midataSettings.userId;
 	},
-		
+
 	/** Get id of resource that has been changed */
 	resourceId : function() {
 		return midataSettings.resourceId;
 	},
-	
+
 	/** Return FHIR message that triggered request as JSON */
 	receiveFHIRMessage : function() {
 		return JSON.parse(fs.readFileSync(0, 'utf8'));
 	},
-	
+
 	/** Send answer to FHIR message back to server */
 	answerFHIRMessage : function(answerJson) {
 		process.stdout.write(JSON.stringify(answerJson));
 	},
-	
+
 	/** Read a FHIR resource */
-	fhirRead : function(authToken, resourceType, id, version) {						
+	fhirRead : function(authToken, resourceType, id, version) {
 		return request({
 			json : true,
 	    	url : midataSettings.server + "/fhir/"+resourceType+"/"+id+(version !== undefined ? "/_history/"+version : ""),
-	    	headers : { "Authorization" : "Bearer "+authToken }
-	    }); 
+	    	headers : { "Authorization" : "Bearer " + authToken, "Accept" : "application/fhir+json; fhirVersion=4.0" }
+	    });
 	},
-	
+
 	/** Search for FHIR resources */
-	fhirSearch : function(authToken, resourceType, params, unbundle) {						
+	fhirSearch : function(authToken, resourceType, params, unbundle) {
 		var req = request({
-			json : true,		    	    	
+			json : true,
 	    	url : midataSettings.server + "/fhir/"+resourceType,
-	    	headers : { "Authorization" : "Bearer "+authToken },
-	    	qs : params	    	
+	    	headers : { "Authorization" : "Bearer "+authToken, "Accept" : "application/fhir+json; fhirVersion=4.0" },
+	    	qs : params
 	    });
 		return unbundle ? unpackBundle(req) : req;
 	},
-	
+
 	/** Create a new FHIR resource */
-	fhirCreate : function(authToken, resource) {						
+	fhirCreate : function(authToken, resource) {
 		return request({
 	    	method : "POST",
 	    	json : true,
 	    	url : midataSettings.server + "/fhir/"+resource.resourceType,
-	    	headers : { "Authorization" : "Bearer "+authToken , "Prefer" : "return=representation" },
+	    	headers : { "Authorization" : "Bearer "+authToken , "Prefer" : "return=representation", "Accept" : "application/fhir+json; fhirVersion=4.0" },
 	    	body : resource
 	    });
 	},
-	
+
 	/** Update a previously read FHIR resource */
 	fhirUpdate : function(authToken, resource) {
 		return request({
 			method : "PUT",
 			json : true,
 			url : midataSettings.server +"/fhir/"+resource.resourceType+"/"+resource.id,
-			headers : { "Authorization" : "Bearer "+authToken, "Prefer" : "return=representation" },
+			headers : { "Authorization" : "Bearer "+authToken, "Prefer" : "return=representation", "Accept" : "application/fhir+json; fhirVersion=4.0"},
 	    	body : resource
 		});
 	},
-	
+
 	/** Send a bundle containing changes to the server */
-	fhirTransaction : function(authToken, bundle) {							    	   
-		return request({  	    	
+	fhirTransaction : function(authToken, bundle) {
+		return request({
 		    	method : "POST",
 		    	json : true,
 		    	url : midataSettings.server + "/fhir",
-		    	headers : { "Authorization" : "Bearer "+authToken },
-		    	body : bundle	    	
-		    
-	    });		
+		    	headers : { "Authorization" : "Bearer "+authToken, "Accept" : "application/fhir+json; fhirVersion=4.0" },
+		    	body : bundle
+
+	    });
 	}
-		 				
+
 };
